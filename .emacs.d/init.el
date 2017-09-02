91327be99f7561a5603b3e15a299a03ac6db9e39
YouTakaoka <pinokions009@gmail.com>
2017-09-02 17:56:02 +0900

    updated init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,7 +4,7 @@
 (setq load-path (cons "~/.emacs.d/.cask" load-path))
 
 ;;; el-get settings
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(add-to-list 'load-path "~/.emacs.d/el-get")
 
 (unless (require 'el-get nil t)
   (url-retrieve
@@ -149,8 +149,12 @@
 ;; flycheck
 (el-get-bundle flycheck)
 
+;; flycheck-java
+(el-get-bundle flycheck-java)
+
 ;;;;;;;;;;;;;;;;;;;;; flycheck settings ;;;;;;;;;;;;;;
 (require 'flycheck)
+(require 'flycheck-java)
 
 ;; C++ mode
 (add-hook 'c-mode-common-hook 'flycheck-mode)
