ec00fdbab5bddd2a7fa5c8e32b5d343a46d6589e
Yu Takaoka <pinokions009@gmail.com>
2019-02-27 16:08:17 +0900

    changed color of error highlights

--- .emacs.d/custom_setttings.el
+++ .emacs.d/custom_setttings.el
@@ -10,4 +10,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(flycheck-error ((t (:inherit error :background "yellow" :foreground "red" :underline "black")))))
